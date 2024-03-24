96c34c10c99059c2c36905037ae59bedf27dcf3c
Ring Madat <smithemma329@gmail.com>
2024-03-24 20:52:02 +0300

    main

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,51 @@
 import React from "react";
+import "../styles.css"
+import styled from 'styled-components';
+import { GoHomeFill } from "react-icons/go";
+import { LuSearch } from "react-icons/lu";
+
+
+
+
+const Section = styled.section`
+    width:100%;
+`
+const Container  = styled.div`
+    width:100%;
+
+`
+const LeftWrapper = styled.div`
+    width:100%;
+`
+const RightWrapper = styled.div`
+    width:100%;
+`
+
 
 function Navbar() {
     return(
         <>
-        
+            <Section>
+                <Container>
+                    <LeftWrapper>
+                        <div>
+                            <div className="leftWrapper-navbar">
+                                <h2> <span><GoHomeFill /></span> Home</h2>
+                                <h3><span><LuSearch /></span>Search </h3>
+                            </div>
+                            <div className="leftWrapper-lower">
+                                <div className="leftWrapper-lower-container">
+
+                                </div>
+                            </div>
+                        </div>
+                    </LeftWrapper>
+
+                    <RightWrapper>
+
+                    </RightWrapper>
+                </Container>    
+            </Section>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
   </React.StrictMode>
 );
 reportWebVitals();
